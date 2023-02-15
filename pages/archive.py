2d726b663552ce0bbf7282876f59511ddec36575
Isolde Glissenaar <isolde.glissenaar@ziggo.nl>
2023-02-15 12:13:43 +0000

    add SIT figure to archive

--- pages/archive.py
+++ pages/archive.py
@@ -31,20 +31,20 @@ if option=='Sea ice thickness':
     sit = xr.open_dataset(direc+'predic_sit_19932020_'+f"{m:02}"+'.nc')
     y = np.where(sit.year==year)[0][0]
 
-#     fig=plt.figure(dpi=200)
-#     ax = plt.axes(projection=ccrs.Orthographic(central_longitude=-99, central_latitude=70, globe=None))
-#     ax.coastlines(resolution='50m',linewidth=0.5)
-#     ax.set_extent([-140,-57,62,84],crs=ccrs.PlateCarree())
-#     ax.gridlines(linewidth=0.3, color='k', alpha=0.5, linestyle=':')
-#     im = plt.scatter(sit.lon, sit.lat, c=sit.sit_mean[y,:].values, cmap='Spectral_r',vmin=0,vmax=3,s=9, transform=ccrs.PlateCarree())
-#     ax.add_feature(land_50m, facecolor='#eeeeee')
-#     cbar = fig.colorbar(im, ax=ax, fraction=0.029, pad=0.04,extend='max')
-#     cbar.ax.locator_params(nbins=7)
-#     cbar.ax.tick_params(labelsize=8)
-#     cbar.set_label(label='m',fontsize=10)
-#     plt.title(f'Proxy sea ice thickness ({calendar.month_name[m]} {str(year)})')
+    fig=plt.figure(dpi=200)
+    ax = plt.axes(projection=ccrs.Orthographic(central_longitude=-99, central_latitude=70, globe=None))
+    ax.coastlines(resolution='50m',linewidth=0.5)
+    ax.set_extent([-140,-57,62,84],crs=ccrs.PlateCarree())
+    ax.gridlines(linewidth=0.3, color='k', alpha=0.5, linestyle=':')
+    im = plt.scatter(sit.lon, sit.lat, c=sit.sit_mean[y,:].values, cmap='Spectral_r',vmin=0,vmax=3,s=9, transform=ccrs.PlateCarree())
+    ax.add_feature(land_50m, facecolor='#eeeeee')
+    cbar = fig.colorbar(im, ax=ax, fraction=0.029, pad=0.04,extend='max')
+    cbar.ax.locator_params(nbins=7)
+    cbar.ax.tick_params(labelsize=8)
+    cbar.set_label(label='m',fontsize=10)
+    plt.title(f'Proxy sea ice thickness ({calendar.month_name[m]} {str(year)})')
 
-#     st.pyplot(fig=fig)
+    st.pyplot(fig=fig)
 
 
 
