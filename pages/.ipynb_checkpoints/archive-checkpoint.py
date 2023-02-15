7ed1a4b33cce7fc3bbab4d512a5690002007856f
Isolde Glissenaar <isolde.glissenaar@ziggo.nl>
2023-02-15 10:14:32 +0000

    add operational

--- pages/.ipynb_checkpoints/archive-checkpoint.py
+++ pages/.ipynb_checkpoints/archive-checkpoint.py
@@ -11,86 +11,84 @@ land_50m = cfeature.NaturalEarthFeature('physical', 'land', '50m')
 st.header('Proxy SIT in Canadian Arctic')
 st.write("Plot proxy sea ice thickness from a Random Forest Regression using CIS ice charts and scatterometer data. Use the panels on the left to select a year (1992-2020) and a month (November-April)")
 
-# password = st.text_input('Password')
 
-# if password=='Bristol':
 
-#     option = st.sidebar.selectbox(
-#         'What would you like to plot?',
-#         ('Sea ice thickness', 'Trends'))
+option = st.sidebar.selectbox(
+    'What would you like to plot?',
+    ('Sea ice thickness', 'Trends'))
 
-#     if option=='Sea ice thickness':
-#         st.sidebar.write('Select year and month to plot')
-#         m = st.sidebar.select_slider('Month', options=([11,12,1,2,3,4]), help='Select a month to display')
-#         if m<4:
-#             year = st.sidebar.select_slider('Year (1993-2020)', options=(np.arange(1993,2021,1)), help='Select a year to display')
-#         else:
-#             year = st.sidebar.select_slider('Year (1992-2020)', options=(np.arange(1992,2021,1)), help='Select a year to display')
+if option=='Sea ice thickness':
+    st.sidebar.write('Select year and month to plot')
+    m = st.sidebar.select_slider('Month', options=([11,12,1,2,3,4]), help='Select a month to display')
+    if m<4:
+        year = st.sidebar.select_slider('Year (1993-2020)', options=(np.arange(1993,2021,1)), help='Select a year to display')
+    else:
+        year = st.sidebar.select_slider('Year (1992-2020)', options=(np.arange(1992,2021,1)), help='Select a year to display')
 
 
 
-#         direc = './data/'
-#         sit = xr.open_dataset(direc+'predic_sit_19932020_'+f"{m:02}"+'.nc')
-#         y = np.where(sit.year==year)[0][0]
+    direc = './data/'
+    sit = xr.open_dataset(direc+'predic_sit_19932020_'+f"{m:02}"+'.nc')
+    y = np.where(sit.year==year)[0][0]
 
-#         fig=plt.figure(dpi=200)
-#         ax = plt.axes(projection=ccrs.Orthographic(central_longitude=-99, central_latitude=70, globe=None))
-#         ax.coastlines(resolution='50m',linewidth=0.5)
-#         ax.set_extent([-140,-57,62,84],crs=ccrs.PlateCarree())
-#         ax.gridlines(linewidth=0.3, color='k', alpha=0.5, linestyle=':')
-#         im = plt.scatter(sit.lon, sit.lat, c=sit.sit_mean[y,:].values, cmap='Spectral_r',vmin=0,vmax=3,s=6, transform=ccrs.PlateCarree())
-#         ax.add_feature(land_50m, facecolor='#eeeeee')
-#         cbar = fig.colorbar(im, ax=ax, fraction=0.029, pad=0.04,extend='max')
-#         cbar.ax.locator_params(nbins=7)
-#         cbar.ax.tick_params(labelsize=8)
-#         cbar.set_label(label='m',fontsize=10)
-#         plt.title(f'Proxy sea ice thickness ({calendar.month_name[m]} {str(year)})')
+    fig=plt.figure(dpi=200)
+    ax = plt.axes(projection=ccrs.Orthographic(central_longitude=-99, central_latitude=70, globe=None))
+    ax.coastlines(resolution='50m',linewidth=0.5)
+    ax.set_extent([-140,-57,62,84],crs=ccrs.PlateCarree())
+    ax.gridlines(linewidth=0.3, color='k', alpha=0.5, linestyle=':')
+    im = plt.scatter(sit.lon, sit.lat, c=sit.sit_mean[y,:].values, cmap='Spectral_r',vmin=0,vmax=3,s=6, transform=ccrs.PlateCarree())
+    ax.add_feature(land_50m, facecolor='#eeeeee')
+    cbar = fig.colorbar(im, ax=ax, fraction=0.029, pad=0.04,extend='max')
+    cbar.ax.locator_params(nbins=7)
+    cbar.ax.tick_params(labelsize=8)
+    cbar.set_label(label='m',fontsize=10)
+    plt.title(f'Proxy sea ice thickness ({calendar.month_name[m]} {str(year)})')
 
-#         st.pyplot(fig=fig)
-
-
-
-#     if option=='Trends':
-#         st.sidebar.write('Select starting and end year and month to plot')
-#         m = st.sidebar.select_slider('Month', options=([11,12,1,2,3,4]), help='Select a month to display')
-#         if m<4:
-#             year_1,year_2 = st.sidebar.select_slider('Range years (1993-2020)', options=(np.arange(1993,2021,1)), value=[1993,2020], help='Select a starting and ending year for your trend plot')  
-#         else:
-#             year_1, year_2 = st.sidebar.select_slider('Range years (1992-2020)', options=(np.arange(1992,2021,1)), value=[1992,2020], help='Select a starting and ending year for your trend plot') 
-
-#         direc = './data/'
-#         sit = xr.open_dataset(direc+'predic_sit_19932020_'+f"{m:02}"+'.nc')
-#         sit = sit.drop(1997, dim='year')
-#         if m==12:
-#             sit = sit.drop([1993,1994,1995,1996], dim='year')
-
-#         y1 = np.where(sit.year==year_1)[0][0]
-#         y2 = np.where(sit.year==year_2)[0][0]
-
-#         p_value = np.zeros(sit.dims['n'])
-#         trend_sit = np.zeros(sit.dims['n'])
-#         for i in range(sit.dims['n']):
-#             x=sit.year[y1:y2+1]
-#             y=sit.sit_mean[y1:y2+1,i]
-
-#             trend_sit[i], intercept, r_value, p_value[i], std_err = stats.linregress(x, y)
+    st.pyplot(fig=fig)
 
 
-#         fig=plt.figure(dpi=200)
-#         ax = plt.axes(projection=ccrs.Orthographic(central_longitude=-99, central_latitude=70, globe=None))
-#         ax.coastlines(resolution='50m',linewidth=0.5)
-#         ax.set_extent([-140,-57,62,84],crs=ccrs.PlateCarree())
-#         ax.gridlines(linewidth=0.3, color='k', alpha=0.5, linestyle=':')
-#         im = plt.scatter(sit.lon, sit.lat, c=trend_sit*100, cmap='RdBu',vmin=-3,vmax=3,s=8,transform=ccrs.PlateCarree())
-#         plt.scatter(sit.lon[p_value<0.05], sit.lat[p_value<0.05], facecolors='none',
-#                     s=9, edgecolor='black', linewidth=0.2, transform=ccrs.PlateCarree())
-#         ax.add_feature(land_50m, facecolor='#eeeeee')
-#         cbar = fig.colorbar(im,ax=ax,fraction=0.026, pad=0.04,extend='both')
-#         cbar.ax.locator_params(nbins=7)
-#         cbar.ax.tick_params(labelsize=8)
-#         cbar.set_label(label='cm/yr',fontsize=10,fontname='Arial')
-#         plt.title(f'SIT trend ({calendar.month_name[int(m)]} {str(year_1)}-{str(year_2)})', fontsize=10, fontname='Arial')
 
-#         st.pyplot(fig=fig)
+if option=='Trends':
+    st.sidebar.write('Select starting and end year and month to plot')
+    m = st.sidebar.select_slider('Month', options=([11,12,1,2,3,4]), help='Select a month to display')
+    if m<4:
+        year_1,year_2 = st.sidebar.select_slider('Range years (1993-2020)', options=(np.arange(1993,2021,1)), value=[1993,2020], help='Select a starting and ending year for your trend plot')  
+    else:
+        year_1, year_2 = st.sidebar.select_slider('Range years (1992-2020)', options=(np.arange(1992,2021,1)), value=[1992,2020], help='Select a starting and ending year for your trend plot') 
+
+    direc = './data/'
+    sit = xr.open_dataset(direc+'predic_sit_19932020_'+f"{m:02}"+'.nc')
+    sit = sit.drop(1997, dim='year')
+    if m==12:
+        sit = sit.drop([1993,1994,1995,1996], dim='year')
+
+    y1 = np.where(sit.year==year_1)[0][0]
+    y2 = np.where(sit.year==year_2)[0][0]
+
+    p_value = np.zeros(sit.dims['n'])
+    trend_sit = np.zeros(sit.dims['n'])
+    for i in range(sit.dims['n']):
+        x=sit.year[y1:y2+1]
+        y=sit.sit_mean[y1:y2+1,i]
+
+        trend_sit[i], intercept, r_value, p_value[i], std_err = stats.linregress(x, y)
+
+
+    fig=plt.figure(dpi=200)
+    ax = plt.axes(projection=ccrs.Orthographic(central_longitude=-99, central_latitude=70, globe=None))
+    ax.coastlines(resolution='50m',linewidth=0.5)
+    ax.set_extent([-140,-57,62,84],crs=ccrs.PlateCarree())
+    ax.gridlines(linewidth=0.3, color='k', alpha=0.5, linestyle=':')
+    im = plt.scatter(sit.lon, sit.lat, c=trend_sit*100, cmap='RdBu',vmin=-3,vmax=3,s=8,transform=ccrs.PlateCarree())
+    plt.scatter(sit.lon[p_value<0.05], sit.lat[p_value<0.05], facecolors='none',
+                s=9, edgecolor='black', linewidth=0.2, transform=ccrs.PlateCarree())
+    ax.add_feature(land_50m, facecolor='#eeeeee')
+    cbar = fig.colorbar(im,ax=ax,fraction=0.026, pad=0.04,extend='both')
+    cbar.ax.locator_params(nbins=7)
+    cbar.ax.tick_params(labelsize=8)
+    cbar.set_label(label='cm/yr',fontsize=10,fontname='Arial')
+    plt.title(f'SIT trend ({calendar.month_name[int(m)]} {str(year_1)}-{str(year_2)})', fontsize=10, fontname='Arial')
+
+    st.pyplot(fig=fig)
 
 
--- pages/operational.py
+++ pages/operational.py
@@ -1,58 +1,67 @@
 import streamlit as st
-#import matplotlib.pyplot as plt
 
-st.header('Opertational SIT proxy')
-st.write("Work in progress: this page will display this week's ice thickness soon...")
+st.header('Operational SIT proxy')
+st.write("Latest ice conditions:")
+
+#===================================
+
+import pandas as pd
+import matplotlib.pyplot as plt #if using matplotlib
+import plotly.express as px #if using plotly
+import geopandas as gpd
+import datetime
+import calendar
+
+#Get date of last ice chart
+today = datetime.date.today()
+monday_date = today + datetime.timedelta(days=-today.weekday())
+weekday_no = datetime.datetime.today().weekday()  # Mo=0, Thursday=3
+if weekday_no>=3:  #If Thursday or later in the week
+    date = monday_date
+else:
+    date = monday_date - datetime.timedelta(weeks=1)
+date = f"{date.day:02}"+f"{date.month:02}"+str(date.year)
+
+#set up the file path and read the shapefile data
+#Western Arctic
+fp = 'C:/Users/zq19140/OneDrive - University of Bristol/Documents/Projects/JGI_seedcorn/new_chart/'+date+'_CEXPREA_withsit.shp'
+data = gpd.read_file(fp)
+data = data.to_crs(epsg=4326)
 
 
+#set up the file path and read the shapefile data
+#Eastern Arctic
+fp = 'C:/Users/zq19140/OneDrive - University of Bristol/Documents/Projects/JGI_seedcorn/new_chart/'+date+'_CEXPRWA_withsit.shp'
+data2 = gpd.read_file(fp)
+data2 = data2.to_crs(epsg=4326)
 
 
+#merged = pd.concat([data, data2])
 
 
+#Create figure
+fig = px.choropleth(data, geojson=data.geometry, 
+                    locations=data.index, color="sit",
+                    width=1000,
+                    height=500,
+                   color_continuous_scale="Spectral_r")
+fig2 = px.choropleth(data2, geojson=data2.geometry, 
+                    locations=data2.index, color="sit",
+                    width=1000, height=500,
+                   color_continuous_scale="Spectral_r")
+fig.add_trace(fig2.data[0])
+fig.update_geos(fitbounds="locations", visible=True)
+fig.update_geos(projection_type="orthographic",resolution=50)
+fig.update_layout(
+    title_text=f'SIT {date[0:2]} {calendar.month_name[int(date[2:4])]} {str(date[4:8])}'
+)
+fig.update(layout = dict(title=dict(x=0.5)))
+fig.update_layout(
+    margin={"r":0,"t":30,"l":10,"b":10},
+    coloraxis_colorbar={
+        'title':'SIT [m]'})
 
+st.plotly_chart(fig, use_container_width=True)
 
-
-
-#m = 4
-#year = 2000
-
-#direc = 'C:/Users/zq19140/OneDrive - University of Bristol/Documents/Projects/icecharts_thickness/data/processed/predicted_sit_edit/'
-#sit = xr.open_dataset(direc+'predic_sit_19932020_'+f"{m:02}"+'.nc')
-#y = np.where(sit.year==year)[0][0]
-
-#d = {'lat': sit.lat.values, 
-#     'lon': sit.lon.values,
-#     'sit': sit.sit_mean[y,:].values}
-#df = pd.DataFrame(data=d)
-#df = df.dropna()
-#    
-#def colorsscatter(df):
-#    cmap = matplotlib.cm.get_cmap('Spectral_r')
-#    color = cmap(df.sit.values/3)*255
-#    return(color[:,0],color[:,1],color[:,2])#
-
-#df['r'],df['g'],df['b'] = colorsscatter(df)
-
-#import pydeck as pdk
-
-#st.pydeck_chart(pdk.Deck(
-#    map_style=None,
-#    initial_view_state=pdk.ViewState(
-#        latitude=76,
-#        longitude=-116,
-#        zoom=1.5,
-#    ),
-#    layers=[        
-#        pdk.Layer(
-#           'ScatterplotLayer',
-#            data=df,
-#            get_position='[lon, lat]',                
-#            get_fill_color=['r', 'g', 'b'],
-#            get_line_color=[0, 0, 0],
-#            get_radius=30000,
-#            opacity=0.8
-#        ),
-#    ],
-#))
 
 
