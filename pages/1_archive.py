584c4b809c73bb760be6a55033c0bbf385abff32
IsoldeGlissenaar <43541832+IsoldeGlissenaar@users.noreply.github.com>
2023-03-01 11:45:41 +0000

    comment out archive

--- pages/1_archive.py
+++ pages/1_archive.py
@@ -18,39 +18,39 @@ st.write("Plot proxy sea ice thickness from a Random Forest Regression using CIS
 
 
 
-option = st.selectbox(
-    'What would you like to plot?',
-    ('Sea ice thickness', 'Trends'))
+# option = st.selectbox(
+#     'What would you like to plot?',
+#     ('Sea ice thickness', 'Trends'))
 
-if option=='Sea ice thickness':
+# if option=='Sea ice thickness':
     
-    st.write('Select year and month to plot')
-    m = st.select_slider('Month', options=([11,12,1,2,3,4]), help='Select a month to display')
-    year = st.select_slider('Year (1996-2020)', 
-                                    options=(np.arange(1996,2021,1)), 
-                                    help='Select a year to display')
+#     st.write('Select year and month to plot')
+#     m = st.select_slider('Month', options=([11,12,1,2,3,4]), help='Select a month to display')
+#     year = st.select_slider('Year (1996-2020)', 
+#                                     options=(np.arange(1996,2021,1)), 
+#                                     help='Select a year to display')
 
 
-    figs.sitfig(m,year)
+#     figs.sitfig(m,year)
 
 
 
 
-if option=='Trends':
-    st.write('Select starting and end year and month to plot')
-    m = st.select_slider('Month', options=([11,12,1,2,3,4]), help='Select a month to display')
-    year_1, year_2 = st.select_slider('Range years (1996-2020)', 
-                                        options=(np.arange(1996,2021,1)),
-                                        value=[1996,2020], 
-                                        help='Select a starting and ending year for your trend plot') 
+# if option=='Trends':
+#     st.write('Select starting and end year and month to plot')
+#     m = st.select_slider('Month', options=([11,12,1,2,3,4]), help='Select a month to display')
+#     year_1, year_2 = st.select_slider('Range years (1996-2020)', 
+#                                         options=(np.arange(1996,2021,1)),
+#                                         value=[1996,2020], 
+#                                         help='Select a starting and ending year for your trend plot') 
 
-    timeline.sittrendfig(m,year_1,year_2)
+#     timeline.sittrendfig(m,year_1,year_2)
 
 
-    # Plot timeline SIT for a given region
-    st.write('Plot timeline and trend SIT for a given region:')
-    location = st.selectbox(
-        'Location',
-        ('Full area', 'Arctic Ocean Periphery', 'Baffin Bay', 'Beaufort Sea', 'Parry Channel'))
+#     # Plot timeline SIT for a given region
+#     st.write('Plot timeline and trend SIT for a given region:')
+#     location = st.selectbox(
+#         'Location',
+#         ('Full area', 'Arctic Ocean Periphery', 'Baffin Bay', 'Beaufort Sea', 'Parry Channel'))
 
-    timeline.timeline(m,year_1,year_2, location)
+#     timeline.timeline(m,year_1,year_2, location)
