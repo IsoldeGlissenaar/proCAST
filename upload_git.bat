866e549841fdb7a8d6c429a04a07dbb9681384b0
IsoldeGlissenaar <43541832+IsoldeGlissenaar@users.noreply.github.com>
2023-03-20 12:40:35 +0000

    remove 27 feb

--- upload_git.bat
+++ upload_git.bat
@@ -1,5 +1,6 @@
 cd "C:\Users\zq19140\OneDrive - University of Bristol\Documents\GitHub\proCAST"
-git checkout dev
-git add /data/operational/
+git add ./data/operational/
 git commit -m "new operational chart"
-git push origin master
+git push 
+
+pause
