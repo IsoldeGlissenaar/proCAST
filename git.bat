2413be30cde7f2db9e8c63968740ed17196f37ef
IsoldeGlissenaar <43541832+IsoldeGlissenaar@users.noreply.github.com>
2023-03-20 10:54:36 +0000

    add git batch file

---
+++ git.bat
@@ -0,0 +1,5 @@
+cd "C:\Users\zq19140\OneDrive - University of Bristol\Documents\GitHub\proCAST"
+git checkout dev
+git add /data/operational/
+git commit -m "new operational chart"
+git push origin master
