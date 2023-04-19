e49809b11655e11ae0a590a3ed70b9460999b48c
Isolde Glissenaar <isolde.glissenaar@ziggo.nl>
2023-04-19 10:07:26 +0100

    add caption

--- home.py
+++ home.py
@@ -6,6 +6,6 @@ st.write("A Random Forest Regression model that creates a proxy product for Cana
 
 st.write("A scientific paper about the proxy SIT product and it's validation and first results is under review and a preprint is available [here](https://egusphere.copernicus.org/preprints/2023/egusphere-2023-269/).")
 
-st.image("image/ship_arctic.jpg")
+st.image("image/ship_arctic.jpg", caption="Photo Credit: Patrick Kelley, U.S. Coast Guard")
 
 st.write("When using the data please use these citations:  \n Glissenaar, I., Landy, J., Babb, D., Dawson, G. & Howell, S. (2023). IsoldeGlissenaar/CanadianSITproxy: Proxy SIT Canadian Arctic - dataset (v0.3) [Data set]. Zenodo. https://doi.org/10.5281/zenodo.7712970  \nGlissenaar, I. A., Landy, J. C., Babb, D. G., Dawson, G. J., and Howell, S. E. L.: A long-term proxy for sea ice thickness in the Canadian Arctic: 1996–2020, EGUsphere [preprint], https://doi.org/10.5194/egusphere-2023-269, 2023.")
